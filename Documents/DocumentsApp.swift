d2cf15f870a286dd15247f7df02b1b1e843c95a2
snewell-WS <snewell234@gmail.com>
2023-09-04 11:42:52 -0500

    Initial Commit

---
+++ Documents/DocumentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentsApp.swift
+//  Documents
+//
+//  Created by Sean Newell on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DocumentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
